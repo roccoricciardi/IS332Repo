1a96546c70fe64e8667c1e353f54f53c5dae3d64
roccoricciardi <rar37@njit.edu>
2014-02-08 17:31:57 -0500

    Optimizing navigation for small screens

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+<ul  id="nav-anchors" class="nav-anchors">
+  <li><a href="#nav" id="menu-anchor">Menu</a></li>
+  <li><a href="#search" id="search-anchor">Search</a></li>
+</ul>
+<form id="search" action="#" method="post" class="search reveal">
+  <fieldset>
+    <legend>Search the Site</legend>
+    <input type="search" placeholder="Search Store" />
+    <input type="submit" value="Search" />
+  </fieldset>
+</form>
+<nav id="nav" class="nav reveal">
+  <ul role="navigation">
+    <li><a href="#">T-shirts</a></li>
+    <li><a href="#">Hoodies</a></li>
+    <li><a href="#">Pants</a></li>
+  </ul>
+</nav>
